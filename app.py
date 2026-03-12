3e1a60227ce8aabc1e9ce33aca217d49fdc54cf2
evangelistak783-spec <evangelistak783@gmail.com>
2026-03-12 13:48:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, jsonify, render_template
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
-
-# This is our "database" for now
-student_data = {
-    "name": "Your Name",
-    "grade": 10,
-    "section": "Zechariah"
-}
 
 @app.route('/')
 def home():
-    # render_template looks inside the /templates folder automatically
-    return render_template('index.html', student=student_data)
+return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
-    return jsonify(student_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+return jsonify({
+"name": "Your Name",
+"grade": 10,
+"section": "Zechariah"
+})
